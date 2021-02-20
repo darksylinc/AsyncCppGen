3ca1482306e27ec5d96993f24e6e38c71d39299e
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2021-02-20 15:05:35 -0300

    Renamed ASYNC_SWITCH_CALL to ASYNC_SWITCH_CALL_STACK

--- bin/Data/LuaBridgeSwitch_Template01.cpp
+++ bin/Data/LuaBridgeSwitch_Template01.cpp
@@ -2,5 +2,5 @@
 void {0}::{1}{2}( {4} )
 {{
 	Async{5}_{3} msg( {6} );
-	m_logicSystem.queueSendMessage( m_logicSystem.getGraphicsSystem(), Mq::ASYNC_SWITCH_CALL, msg );
+	m_logicSystem.queueSendMessage( m_logicSystem.getGraphicsSystem(), Mq::ASYNC_SWITCH_CALL_STACK, msg );
 }}
